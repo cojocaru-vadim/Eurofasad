19b811eb73195875d7d7bc202b0e3d6c6587ffb8
vadim <vadim@vadim-laptop>
2012-02-11 11:53:38 +0200

    home

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -6,6 +6,7 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
     protected function _initLocale()
     {
 
+
         $locale = null;
         $session = new Zend_Session_Namespace('accounts');
 
